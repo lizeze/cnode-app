232ab47c7e2fe6c0899d81126d0267ab6c7a9000
zeze.li <lizeze.boy@gmail.com>
2019-02-18 11:47:09 +0800

    add

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Index from '@/page/home/index';
 import Detail from '@/page/home/detail';
-import MyFlex from '@/page/flex/MyFlex';
+import MyFlex from '@/page/home/index';
 Vue.use(Router)
 
 export default new Router({
@@ -17,11 +16,6 @@ export default new Router({
       path: '/detail/:id',
       name: 'detail',
       component: Detail
-    },
-    {
-      path: '/flex',
-      name: 'MyFlex',
-      component: Index
     }
   ]
 })
